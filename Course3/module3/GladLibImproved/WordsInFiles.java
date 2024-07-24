65e22c992426d7f78c201e9493e6bcb437d3ffc4
Vaishaliyadavv <vaishali.yadavv09@gmail.com>
2024-07-24 19:36:34 +0530

    add files to GladLibImproved

---
+++ Course3/module3/GladLibImproved/WordsInFiles.java
@@ -0,0 +1,86 @@
+
+/**
+ * Write a description of WordsInFiles here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.io.File;
+import edu.duke.*;
+import java.util.*;
+public class WordsInFiles {
+    private HashMap<String , ArrayList<String>> wordFileMap;
+    
+    public WordsInFiles(){
+        wordFileMap = new HashMap<String , ArrayList<String>>();
+    }
+    
+    private void addWordsFromFile(File f){
+        FileResource fr  = new FileResource(f);
+        String fileName = f.getName();
+        for(String word : fr.words()){
+            if(!wordFileMap.containsKey(word)){
+                wordFileMap.put(word,new ArrayList<String>());
+            }
+            ArrayList<String> fileList = wordFileMap.get(word);
+            if(!fileList.contains(fileName)){
+                fileList.add(fileName);
+            }
+        }
+    }
+    
+    public void buildWordFileMap(){
+        wordFileMap.clear();
+        DirectoryResource dr = new DirectoryResource();
+        for(File f : dr.selectedFiles()){
+            addWordsFromFile(f);
+        }
+    }
+    
+    public int maxNumber(){
+        int max = 0;
+        for(ArrayList<String> fileList : wordFileMap.values()){
+            if(fileList.size() > max){
+                max = fileList.size();
+            }
+        }
+        return max;
+    }
+    
+    public ArrayList<String> wordsInNumFiles(int number){
+        ArrayList<String> words = new ArrayList<String>();
+        for(String word : wordFileMap.keySet()){
+            ArrayList<String> fileList = wordFileMap.get(word);
+            if(fileList.size() == number){
+                words.add(word);
+            }
+        }
+        return words;
+    }
+    
+    public void printFilesIn(String word){
+        ArrayList<String> fileList = wordFileMap.get(word);
+        if(fileList != null){
+            for(String fileName : fileList){
+                System.out.println(fileName);
+            }
+        }
+        else {
+            System.out.println("word not fount in any file");
+        }
+    }
+    
+     public void tester() {
+        buildWordFileMap();
+        int maxFiles = maxNumber();
+        System.out.println("Maximum number of files any word is in: " + maxFiles);
+
+        ArrayList<String> maxWords = wordsInNumFiles(maxFiles);
+        System.out.println("Words that appear in the maximum number of files (" + maxFiles + "): " + maxWords);
+
+        for (String word : maxWords) {
+            System.out.println("The word '" + word + "' appears in the following files:");
+            printFilesIn(word);
+        }
+    }
+}
